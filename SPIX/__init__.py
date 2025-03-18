ec0f4905b97ac2f3c4b5e488bf080621fd9d7eae
whistle-ch0i <155410914+whistle-ch0i@users.noreply.github.com>
2025-03-18 14:24:00 +0900

    Update __init__.py for simplifying callers

--- SPIX/__init__.py
+++ SPIX/__init__.py
@@ -1,23 +1,19 @@
-# SPIX_module/__init__.py
+# SPIX/__init__.py
 
-from .tiles_embeddings import embeddings, tiles
-from .visualization import plotting
-from .image_processing import smoothing, equalization
-from .superpixel import segmentation
-from .optimization import parameter_selection
-from .analysis import enrichment_analysis, calculate_original_moranI, perform_pseudo_bulk_analysis
-from .utils import utils
+from . import tiles_embeddings as tm
+from . import visualization as pl
+from . import image_processing as ip
+from . import superpixel as sp
+from . import optimization as op
+from . import analysis as an
+from . import utils
 
 __all__ = [
-    'embeddings',
-    'tiles',
-    'plotting',
-    'smoothing',
-    'equalization',
-    'segmentation',
-    'parameter_selection',
-    'enrichment_analysis',
-    'original_moranI',
-    'pseudo_bulk',
+    'tm',
+    'pl',
+    'ip',
+    'sp',
+    'op',
+    'an',
     'utils'
 ]
